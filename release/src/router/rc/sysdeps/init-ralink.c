ee736b1ee1f11a8c5edccf8114068fe07c8f2076
黑猫 <133520@livecn>
2021-05-18 16:40:34 +0800

    修改wifi地区设置

--- release/src/router/rc/sysdeps/init-ralink.c
+++ release/src/router/rc/sysdeps/init-ralink.c
@@ -1666,6 +1666,21 @@ void init_syspara(void)
 	_dprintf("bootloader version: %s\n", nvram_safe_get("blver"));
 	_dprintf("firmware version: %s\n", nvram_safe_get("firmver"));
 
+	// set country from location_code
+	char *loc_code[] = {"DB", "EU", "RU", "US", "CN", 0};
+	char **curr_loc_code = loc_code;
+	for (; *curr_loc_code; *curr_loc_code++)
+		if (nvram_match("location_code", *curr_loc_code)) {
+			nvram_set("wl_country_code", *curr_loc_code);
+			nvram_set("wl0_country_code", *curr_loc_code);
+			nvram_set("wl1_country_code", *curr_loc_code);
+#ifdef RTCONFIG_RALINK_DFS
+			if (!strcmp(*curr_loc_code, "EU"))
+				nvram_set("wl1_IEEE80211H", "1");
+#endif
+			break;
+		}
+
 #if 0
 	dst = txbf_para;
 	int count_0xff = 0;
--- release/src/router/shared/defaults.c
+++ release/src/router/shared/defaults.c
@@ -290,7 +290,7 @@ struct nvram_tuple router_defaults[] = {
 	{ "AUTO_CHANNEL", "1", CKN_STR1, CKN_TYPE_DEFAULT, CKN_ACC_LEVEL_DEFAULT, CKN_ENC_DEFAULT, 0 },	/* 0: real channel; 1: Auto; for RT-N12HP & RT-N14UHP */
 #endif
 #ifdef RTCONFIG_TCODE
-	{ "location_code", "", CKN_STR8, CKN_TYPE_DEFAULT, CKN_ACC_LEVEL_DEFAULT, CKN_ENC_DEFAULT, 0 },	/* 0: real channel; 1: Auto; for RT-N12HP & RT-N14UHP */
+	{ "location_code", "CN", CKN_STR8, CKN_TYPE_DEFAULT, CKN_ACC_LEVEL_DEFAULT, CKN_ENC_DEFAULT, 0 },	/* 0: real channel; 1: Auto; for RT-N12HP & RT-N14UHP */
 #endif
 #ifdef RTCONFIG_RALINK
 	{ "wl_nband", "2", CKN_STR1, CKN_TYPE_DEFAULT, CKN_ACC_LEVEL_DEFAULT, CKN_ENC_DEFAULT, 0 },	/* N-BAND */
