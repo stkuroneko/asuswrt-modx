a55c5e0086d9ad346f19d3f1eb37dfbdb99831fb
whitecloudfly <64976478+whitcloudfly@users.noreply.github.com>
2023-04-01 19:46:44 +0800

    Update m25p80.c
    add w25q512jvfq support

--- release/src-ra-5010/linux/linux-3.10.14.x/drivers/mtd/devices/m25p80.c
+++ release/src-ra-5010/linux/linux-3.10.14.x/drivers/mtd/devices/m25p80.c
@@ -844,6 +844,7 @@ static const struct spi_device_id m25p_ids[] = {
 	{ "w25q80bl", INFO(0xef4014, 0, 64 * 1024,  16, SECT_4K) },
 	{ "w25q128", INFO(0xef4018, 0, 64 * 1024, 256, SECT_4K) },
 	{ "w25q256", INFO(0xef4019, 0, 64 * 1024, 512, SECT_4K) },
+	{ "w25q512jv", INFO(0xef4020, 0, 64 * 1024, 1024, SECT_4K) },
 
 	/* Catalyst / On Semiconductor -- non-JEDEC */
 	{ "cat25c11", CAT25_INFO(  16, 8, 16, 1) },
